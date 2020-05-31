a3b5ff634026bc4666313ce8f57bb90c6bd9d802
J. Trpka <jtrpka0912@gmail.com>
2020-05-31 07:49:41 -0400

    Created the sample shelf, folder, and file models in the mock MongoDB.

--- test/libs/shelf/from-mongodb/files.test.js
+++ test/libs/shelf/from-mongodb/files.test.js
@@ -43,13 +43,73 @@ describe('Files from MongoDB', () => {
 
     it('Throw an error because its missing a shelf', async () => {
         const error = await retrieveFiles();
-        // TODO: Fix this for folders => isString(error.errorCodeMessage)
         assert.isString(error.errorMessage);
         assert.containIgnoreCase(error.errorMessage, 'Shelf was missing in call.');
     });
 
     describe('Find files when showing directories', () => {
-        
+        let novelShelf;
+        before(async () => {
+            // Create some shelves
+            // ===================
+            novelShelf = new Shelf({
+                name: 'Book Shelf',
+                root: ['library', 'books', 'novels'],
+                showDirectories: true,
+                multiFile: false
+            });
+
+            // Create some folders
+            // ===================
+            novelLiterature = new Shelf({
+                name: 'Romance',
+                path: ['library', 'books', 'novels', 'literature']
+            });
+
+            novelDrama = new Shelf({
+                name: 'Drama',
+                path: ['library', 'books', 'novels', 'drama']
+            });
+
+            // Create some files
+            // =================
+            threeKingdoms = new File({
+                type: 'book',
+                name: 'Romance of the Three Kingdoms',
+                path: ['library', 'books', 'novels', 'drama', 'threekingdoms.pdf'],
+                cover: ['covers', 'library', 'books', 'novels', 'drama', 'threekingdoms.jpg'],
+                didRead: false
+            });
+
+            journeyToWest = new File({
+                type: 'book',
+                name: 'Journey to the West',
+                path: ['library', 'books', 'novels', 'literature', 'journeytothewest.pdf'],
+                cover: ['covers', 'library', 'books', 'novels', 'literature', 'journeytothewest.jpg'],
+                didRead: true
+            });
+
+            huckeberryFinn = new File({
+                type: 'book',
+                name: 'Adventures of Huckleberry Finn',
+                path: ['library', 'books', 'novels', 'literature', 'huckberryfinn.pdf'],
+                cover: ['covers', 'library', 'books', 'novels', 'literature', 'huckberryfinn.jpg'],
+                didRead: false
+            });
+
+            await novelShelf.save();
+            await novelLiterature.save();
+            await novelDrama.save();
+            await threeKingdoms.save();
+            await journeyToWest.save();
+            await huckeberryFinn.save();
+        });
+
+        after(async () => {
+            Shelf.deleteMany({});
+            Folder.deleteMany({});
+            File.deleteMany({});
+        });
     });
 
     describe('Find files when not showing directories', () => {
