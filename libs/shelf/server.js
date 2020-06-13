cccf7728f099b2967e05f0a9b1caeed53bce14ad
J. Trpka <jtrpka0912@gmail.com>
2020-06-13 07:11:34 -0400

    Added a mock 'server' to make tests for retrieving files from server.

--- libs/shelf/server.js
+++ libs/shelf/server.js
@@ -2,15 +2,6 @@
 const Folder = require('../../models/folder.model');
 const File = require('../../models/file.model');
 
-/**
- * @function createFolders
- * @description Create folders from a file server to MongoDB.
- * @param { object } shelf 
- */
-const createFolders = (shelf) => {
-    return true;
-};
-
 module.exports = {
     createFolders
 };
