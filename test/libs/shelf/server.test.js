3b352747a58ad208be794947a7cd1745c04a6216
J. Trpka <jtrpka0912@gmail.com>
2020-06-20 09:42:45 -0400

    Added false testing for creating folder documents to MongoDB.

--- test/libs/shelf/server.test.js
+++ test/libs/shelf/server.test.js
@@ -15,7 +15,7 @@ const Shelf = require('../../../models/shelf.model');
 const Folder = require('../../../models/folder.model');
 
 // Libs
-const { retrieveFilesFolders } = require('../../../libs/shelf/server');
+const { retrieveFilesFolders, createFolderToMongoDB } = require('../../../libs/shelf/server');
 
 // Global Variables
 let mongoServer;
@@ -82,24 +82,42 @@ describe('Create and Retrieve Files and Folders through Server to MongoDB', () =
             assert.containIgnoreCase(error.message, 'no such file or directory');
         });
 
-        it('Find the new nodes from the book shelf in MongoDB', async () => {
+        it.skip('Find the new nodes from the book shelf in MongoDB', async () => {
             await retrieveFilesFolders(bookShelf);
 
             // Retrieve the folder count
-            Folder.find().count(function(err, count) {
+            Folder.find().countDocuments(function(err, count) {
                 assert.equal(count, 1);
             });
 
-            Folder.find({ name: 'Sample' }).then((mongoResponse) => {
-                assert.isObject(mongoResponse);
-                assert.equal(mongoResponse.name, 'Samples');
-                assert.equal(Folder.convertPathToString(mongoResponse.path), 'd:/Backend/Nodejs/intranet.bookshelf.nodejs/test/sample-server/Books/Samples');
-            });
+            const count = await Folder.find().countDocuments().exec();
+            assert.equal(count, 1);
+
+            const sampleFolder = await Folder.findOne({ name: 'Sample' }).exec();
+            assert.isObject(sampleFolder);
+            assert.equal(sampleFolder.name, 'Samples');
+            assert.equal(Folder.convertPathToString(sampleFolder.path), 'd:/Backend/Nodejs/intranet.bookshelf.nodejs/test/sample-server/Books/Samples');
         });  
     });
 
     describe('createFolderToMongoDB()', () => {
-        
+        it('Throw an error because it is missing shelf', async () => {
+            const error = await createFolderToMongoDB();
+            // TODO: Should try to add more error testing; prove that error is an error, or something was thrown.
+            assert.containIgnoreCase(error.message, 'Shelf was missing in call');
+        });
+
+        it('Throw an error because it is missing node', async () => {
+            const error = await createFolderToMongoDB(unknownShelf);
+            // TODO: Should try to add more error testing; prove that error is an error, or something was thrown.
+            assert.containIgnoreCase(error.message, 'Node was missing in call');
+        });
+
+        it('Throw an error if folder does not exist in server', async () => {
+            const error = await createFolderToMongoDB(bookShelf, 'Foobar');
+            // TODO: Should try to add more error testing; prove that error is an error, or something was thrown.
+            assert.containIgnoreCase(error.message, 'no such file or directory'); // From FS library
+        });
     });
 
     describe('createFileToMongoDB()', () => {
