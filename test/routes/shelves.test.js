f3e5efe8c192cd145dbb8651781a29df83f609f2
J. Trpka <jtrpka0912@gmail.com>
2020-11-03 07:21:09 -0500

    Refactored the shelves express router.

--- test/routes/shelves.test.js
+++ test/routes/shelves.test.js
@@ -357,62 +357,33 @@ describe('(shelves.test.js) Shelves Router', () => {
     });
 
     describe(`GET - ${endpointURI}/:shelfId/refresh`, () => {
-        const bookShelfId = '5f440bf9e3475949849c292e';
-
         before(async () => {
-
-            let bookShelf = new Shelf({
-                _id: bookShelfId,
-                name: 'Book Shelf',
-                // You will need to adjust the path accordingly
-                root: ['d:', 'Backend', 'Nodejs', 'intranet.bookshelf.nodejs', 'test', 'sample-server', 'Books'],
-                showDirectories: true,
-                multiFile: false
-            });
-            
+            let bookShelf = new Shelf(bookShelfArray);
             await bookShelf.save();
         });
 
-        it('Bad request with a too short ID string (12 characters minimum)', (done) => {
-            chai.request(app).get(`${endpointURI}/blah/refresh`).end((err, res) => {
-                assert.isNotNull(res);
-                // TODO: This is subject to change once I am able to parse MongoDB errors.
-                recognize400(res);
-                recognizeErrorMessage(res, 'Cast to ObjectId failed');
-                done();
-            });
+        it('Bad request with a too short ID string (12 characters minimum)', async() => {
+            const res = await chai.request(app).get(`${endpointURI}/blah/refresh`);
+            assert.isNotNull(res);
+            recognize400(res);
+            recognizeErrorMessage(res, 'Cast to ObjectId failed');
         });
 
-        it('Fail request because it could not find Shelf', (done) => {
-            chai.request(app).get(`${endpointURI}/blahblahblah/refresh`).end((err, res) => {
-                assert.isNotNull(res);
-                recognize404(res);
-                recognizeErrorMessage(res, 'Unable to find shelf with id');
-                done();
-            });
+        it('Fail request because it could not find Shelf', async() => {
+            const res = await chai.request(app).get(`${endpointURI}/blahblahblah/refresh`);
+            assert.isNotNull(res);
+            recognize404(res);
+            recognizeErrorMessage(res, 'Unable to find shelf with id');
         });
 
-        it('Respond with no content if successful', (done) => {
-            chai.request(app).get(`${endpointURI}/${bookShelfId}/refresh`).end((err, res) => {
-                assert.isEmpty(res.body);
-                recognize204(res);
+        it('Respond with no content if successful', async() => {
+            // First retrieve the book shelf we created for this test.
+            const bookShelf = await Shelf.findOne({ name: 'Book Shelf' });
 
-                // Then retrieve a response to get the results from the database
-                chai.request(app).get(`/api/v1/ebooks/shelf/${bookShelfId}`).end((err, res) => {
-                    assert.lengthOf(res.body.breadcrumbs, 0);
-                    assert.lengthOf(res.body.directories, 1);
-                    assert.lengthOf(res.body.files, 0);
-
-                    // Then dive into the folder to get its response
-                    chai.request(app).get(`/api/v1/ebooks/shelf/${bookShelfId}/folder/${res.body.directories[0]._id}`).end((err, res) => {
-                        assert.lengthOf(res.body.breadcrumbs, 1);
-                        assert.lengthOf(res.body.directories, 0);
-                        assert.lengthOf(res.body.files, 2);
-
-                        done();
-                    });
-                });
-            });
+            // Check if it still fetches that book shelf from the endpoint.
+            const res = await chai.request(app).get(`${endpointURI}/${bookShelf._id}/refresh`);
+            assert.isEmpty(res.body);
+            recognize204(res);
         });
     });
 });
